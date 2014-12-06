786fa4a711a2454753e8eb2979f6ac1e7123a49f
migs120 <migs432@yahoo.com>
2014-12-06 12:27:29 +0000

    create-post-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
+  get 'posts/show'
+
+  get 'posts/new'
+
+  get 'posts/edit'
+  
+  resources :posts
+
   get 'welcome/index'
 
   get 'welcome/about'
+  
+  get 'about' => 'welcome#about'
   
   root to: 'welcome#index'
 
