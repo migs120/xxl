2ba5ff2748810e8e7837570784221aa70efe5d41
migs120 <migs432@yahoo.com>
2014-12-14 09:32:43 +0000

    nesting-posts2

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,15 @@
    user.save!
  end
  users = User.all
+
+ # Create Topics
+ 15.times do
+   Topic.create!(
+     name:         Faker::Lorem.sentence,
+     description:  Faker::Lorem.paragraph
+   )
+ end
+ topics = Topic.all
  
  # Note: by calling `User.new` instead of `create`,
  # we create an instance of User which isn't immediately saved to the database.
@@ -24,8 +33,9 @@
  50.times do
    Post.create!(
       user:   users.sample,
+      topic:  topics.sample,
      title:  Faker::Lorem.sentence,
-     body:   Faker::Lorem.paragraph
+      body:   Faker::Lorem.paragraph
    )
  end
  posts = Post.all
@@ -38,12 +48,34 @@
      body: Faker::Lorem.paragraph
    )
  end
-  user = User.first
- user.skip_reconfirmation!
- user.update_attributes!(
-   email: 'migs432@yahoo.com',
-   password: 'helloworld'
+ # Create an admin user
+ admin = User.new(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
  )
+ admin.skip_confirmation!
+ admin.save!
+ 
+ # Create a moderator
+ moderator = User.new(
+   name:     'Moderator User',
+   email:    'moderator@example.com', 
+   password: 'helloworld',
+   role:     'moderator'
+ )
+ moderator.skip_confirmation!
+ moderator.save!
+ 
+ # Create a member
+ member = User.new(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld',
+ )
+ member.skip_confirmation!
+ member.save!
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
