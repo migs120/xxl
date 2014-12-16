a2d957887c9dc2fbe5681517472bc9ba76933c56
migs120 <migs432@yahoo.com>
2014-12-16 04:08:44 +0000

    end of interlude

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
 
   def create
     @topic = Topic.find(params[:topic_id])
-    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    @post = current_user.posts.build(post_params)
     @post.topic = @topic
     authorize @post
 
@@ -44,4 +44,9 @@ class PostsController < ApplicationController
       render :new
     end
   end
+  private
+
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
  
 
   devise_for :users
+  resources :users, only: [:update]
  # get 'posts/index'
 
  # get 'posts/show'
