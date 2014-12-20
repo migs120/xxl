ebf8c5b681cd520d0b047bff32d51cb5181142b6
migs120 <migs432@yahoo.com>
2014-12-20 00:16:49 +0000

    markdown

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def show
+     @post = Post.find(params[:id])
     @topic = Topic.find(params[:topic_id])
-    @post = Post.find(params[:id])
   end
 
   def new
