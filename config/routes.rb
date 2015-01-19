7e41d507c13ac8577296c3b2684ff31496a3c2ae
migs120 <migs432@yahoo.com>
2015-01-19 16:05:19 +0000

    puplic_profiles

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
  
 
   devise_for :users
-  resources :users, only: [:update]
+  resources :users, only: [:update, :show ]
  # get 'posts/index'
 
  # get 'posts/show'
